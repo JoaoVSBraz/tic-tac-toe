41bbc4540acbe91a0695694b0361eda7b77d6b10
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-06 09:10:33 -0300

    create calculateWinner function

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,15 @@ export default function Board() {
     const [xIsNext, setXIsNext] = useState(true)
     const [squares, setSquares] = useState(Array(9).fill(null))
 
+    const winner = calculateWinner(squares)
+    let status
+    winner ? status = `Winner: ${winner}` : status = `Next player: ${(xIsNext ? 'X' : 'O')}`
+
     function handleClick(i){
-        if(squares[i]){
+        if(squares[i] || calculateWinner(squares)){
             return
         }
-        
+
         const nextSquares = squares.slice()
 
         xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = 'O'
@@ -27,6 +31,7 @@ export default function Board() {
     return (
         <>
             <div className="board-row">
+                <div className='status'>{status}</div>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
                 <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
@@ -44,4 +49,24 @@ export default function Board() {
         </>
     );
   }
-  
+
+function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for(let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i]
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a]
+        }
+    }
+    return null
+}
